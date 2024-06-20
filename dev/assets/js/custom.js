45f6d2e5023e7aeab3c0ee72c72bf5948f8cbc7c
Ivyes <iv.s.eshenko@gmail.com>
2024-06-20 12:51:15 +0300

    DO: open mobile menu to click burger, mobile menu

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -1,1 +1,9 @@
-  
+const mobileContainer = document.querySelector('.mobile-container');
+const burger = document.querySelector('.btn-burger');
+
+function addClass() {
+    mobileContainer.classList.toggle('show');
+}
+
+burger.addEventListener('click', addClass);
+
