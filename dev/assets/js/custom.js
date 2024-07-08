26908c2939eff19e315d25617b0dfa19fab62f43
Ivan Yeshenko <iv.s.eshenko@gmail.com>
2024-07-08 21:30:46 +0300

    Done Adventages dekstop and mobile

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -10,7 +10,7 @@ function addClass() {
 
 burger.addEventListener("click", addClass);
 
-const swiper = new Swiper(".banner-slider", {
+const swiperBanner = new Swiper(".banner-slider", {
 	loop: true,
 	spaceBetween: 20,
 
@@ -47,28 +47,35 @@ downloadFile.forEach(item => {
 });
 
 // Swiper advantages
-const advantagesSlideLength = document.querySelectorAll('.advantages .swiper-slide').length
+const advantagesSlideLength = document.querySelectorAll('.advantages .swiper-slide').length;
+console.log(advantagesSlideLength);
 
 const swiperAdvantages = new Swiper('#advantages', {
-    spaceBetween: 12,
-    loop: true,
-    simulateTouch: true,
-    slidesPerView: 1,
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-        enabled: true,
-    },
-    breakpoints: {
-        1025: {
-            spaceBetween: 0,
-            loop: false,
-            slidesPerGroup: advantagesSlideLength,
-            simulateTouch: false,
-            pagination: {
-                enabled: false,
-                el: '.swiper-pagination',
-            }
-        }
-    }
-  });
+  	speed: 400,
+	spaceBetween: 12,
+	loop: true,
+	simulateTouch: true,
+	slidesPerView: 1,
+
+	pagination: {
+		el: '.swiper-pagination',
+		enabled: true,
+		clickable: true,
+	},
+
+	breakpoints: {
+		1025: {
+			spaceBetween: 0,
+			loop: false,
+			slidesPerView: 4,
+			slidesPerGroup: advantagesSlideLength,
+			simulateTouch: false,
+
+			pagination: {
+				enabled: false,
+			}
+		}
+	}
+});
+
+
